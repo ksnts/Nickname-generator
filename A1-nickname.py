af61226c6de4df502f5aa9083c4819e620d0933d
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-11-04 22:43:38 +0800

    Complete Code

---
+++ A1-nickname.py
@@ -0,0 +1,17 @@
+#Assignment 1
+
+#INSTRUCTIONS:
+
+#Create a program that will print your nickname using only asterisk character (*)
+
+print("       *  *      *")
+print("       *  *     *")
+print("       *  *    *")
+print("       *  *   *")
+print("       *  *  *")
+print("       *  *  *")
+print(" *     *  *    *")
+print(" *     *  *     *")
+print("  *  *    *       *")
+
+
